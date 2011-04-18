46093477c15dea7d609a24a562ef8df303c49b3c
Marek Potociar <marek.potociar@oracle.com>
2011-04-18 22:28:06 +0200

    Added client side state definitions

--- src/jax-rs-api/src/main/java/javax/ws/rs/core/Response.java
+++ src/jax-rs-api/src/main/java/javax/ws/rs/core/Response.java
@@ -93,7 +93,9 @@ public abstract class Response {
      * {@link javax.ws.rs.ext.RuntimeDelegate#createHeaderDelegate(java.lang.Class)}
      * for the class of the value or using the values {@code toString} method if a 
      * header delegate is not available.
-     * 
+     *
+     * TODO deprecate and introduce getHeaders() instead.
+     *
      * @return response metadata as a map
      */
     public abstract MultivaluedMap<String, Object> getMetadata();
@@ -651,9 +653,21 @@ public abstract class Response {
          */
         NO_CONTENT(204, "No Content"),
         /**
+         * 205 Reset Content, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.2.6">HTTP/1.1 documentation</a>}.
+         */
+        RESET_CONTENT(205, "Reset Content"),
+        /**
+         * 206 Reset Content, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.2.7">HTTP/1.1 documentation</a>}.
+         */
+        PARTIAL_CONTENT(206, "Partial Content"),
+        /**
          * 301 Moved Permanently, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.3.2">HTTP/1.1 documentation</a>}.
          */
         MOVED_PERMANENTLY(301, "Moved Permanently"),
+        /**
+         * 302 Found, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.3.3">HTTP/1.1 documentation</a>}.
+         */
+        FOUND(302, "Found"),
         /**
          * 303 See Other, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.3.4">HTTP/1.1 documentation</a>}.
          */
@@ -662,6 +676,10 @@ public abstract class Response {
          * 304 Not Modified, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.3.5">HTTP/1.1 documentation</a>}.
          */
         NOT_MODIFIED(304, "Not Modified"),
+        /**
+         * 305 Use Proxy, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.3.6">HTTP/1.1 documentation</a>}.
+         */
+        USE_PROXY(305, "Use Proxy"),
         /**
          * 307 Temporary Redirect, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.3.8">HTTP/1.1 documentation</a>}.
          */
@@ -675,6 +693,10 @@ public abstract class Response {
          */
         UNAUTHORIZED(401, "Unauthorized"),
         /**
+         * 402 Payment Required, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.4.3">HTTP/1.1 documentation</a>}.
+         */
+        PAYMENT_REQUIRED(402, "Payment Required"),
+        /**
          * 403 Forbidden, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.4.4">HTTP/1.1 documentation</a>}.
          */
         FORBIDDEN(403, "Forbidden"),
@@ -683,9 +705,21 @@ public abstract class Response {
          */
         NOT_FOUND(404, "Not Found"),
         /**
+         * 405 Method Not Allowed, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.4.6">HTTP/1.1 documentation</a>}.
+         */
+        METHOD_NOT_ALLOWED(405, "Method Not Allowed"),
+        /**
          * 406 Not Acceptable, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.4.7">HTTP/1.1 documentation</a>}.
          */
         NOT_ACCEPTABLE(406, "Not Acceptable"),
+        /**
+         * 407 Proxy Authentication Required, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.4.8">HTTP/1.1 documentation</a>}.
+         */
+        PROXY_AUTHENTICATION_REQUIRED(407, "Proxy Authentication Required"),
+        /**
+         * 408 Request Timeout, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.4.9">HTTP/1.1 documentation</a>}.
+         */
+        REQUEST_TIMEOUT(408, "Request Timeout"),
         /**
          * 409 Conflict, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.4.10">HTTP/1.1 documentation</a>}.
          */
@@ -695,22 +729,57 @@ public abstract class Response {
          */
         GONE(410, "Gone"),
         /**
+         * 411 Length Required, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.4.12">HTTP/1.1 documentation</a>}.
+         */
+        LENGTH_REQUIRED(411, "Length Required"),
+        /**
          * 412 Precondition Failed, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.4.13">HTTP/1.1 documentation</a>}.
          */
         PRECONDITION_FAILED(412, "Precondition Failed"),
+        /**
+         * 413 Request Entity Too Large, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.4.14">HTTP/1.1 documentation</a>}.
+         */
+        REQUEST_ENTITY_TOO_LARGE(413, "Request Entity Too Large"),
+        /**
+         * 414 Request-URI Too Long, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.4.15">HTTP/1.1 documentation</a>}.
+         */
+        REQUEST_URI_TOO_LONG(414, "Request-URI Too Long"),
         /**
          * 415 Unsupported Media Type, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.4.16">HTTP/1.1 documentation</a>}.
          */
         UNSUPPORTED_MEDIA_TYPE(415, "Unsupported Media Type"),
         /**
+         * 416 Requested Range Not Satisfiable, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.4.17">HTTP/1.1 documentation</a>}.
+         */
+        REQUESTED_RANGE_NOT_SATIFIABLE(416, "Requested Range Not Satisfiable"),
+        /**
+         * 417 Expectation Failed, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.4.18">HTTP/1.1 documentation</a>}.
+         */
+        EXPECTATION_FAILED(417, "Expectation Failed"),
+        /**
          * 500 Internal Server Error, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.5.1">HTTP/1.1 documentation</a>}.
          */
         INTERNAL_SERVER_ERROR(500, "Internal Server Error"),
         /**
+         * 501 Not Implemented, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.5.2">HTTP/1.1 documentation</a>}.
+         */
+        NOT_IMPLEMENTED(501, "Not Implemented"),
+        /**
+         * 502 Bad Gateway, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.5.3">HTTP/1.1 documentation</a>}.
+         */
+        BAD_GATEWAY(502, "Bad Gateway"),
+        /**
          * 503 Service Unavailable, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.5.4">HTTP/1.1 documentation</a>}.
          */
-        SERVICE_UNAVAILABLE(503, "Service Unavailable");
-        
+        SERVICE_UNAVAILABLE(503, "Service Unavailable"),
+        /**
+         * 504 Gateway Timeout, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.5.5">HTTP/1.1 documentation</a>}.
+         */
+        GATEWAY_TIMEOUT(504, "Gateway Timeout"),
+        /**
+         * 505 HTTP Version Not Supported, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.5.6">HTTP/1.1 documentation</a>}.
+         */
+        HTTP_VERSION_NOT_SUPPORTED(505, "HTTP Version Not Supported");
         private final int code;
         private final String reason;
         private Family family;
