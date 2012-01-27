cf017808f06ab67f9a0fd4dc06b02fc7236b8d4e
Marek Potociar <marek.potociar@oracle.com>
2012-01-27 10:22:12 +0100

    License header year fixed.

--- src/jax-rs-api/src/main/java/javax/ws/rs/ValidationException.java
+++ src/jax-rs-api/src/main/java/javax/ws/rs/ValidationException.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2010-2011 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010-2012 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -45,7 +45,7 @@ import javax.ws.rs.core.Response;
 
 /**
  * Exception class used by JAX-RS implementations to report constraint
- * validations. 
+ * validations.
  *
  * @author Santiago.PericasGeertsen (Santiago.PericasGeertsen@oracle.com)
  * @since 2.0
