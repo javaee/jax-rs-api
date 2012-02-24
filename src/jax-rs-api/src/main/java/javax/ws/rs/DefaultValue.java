4b871c77b69f131be563d67a7d1370d7aefddf54
Marek Potociar <marek.potociar@oracle.com>
2012-02-24 13:31:49 +0100

    Re-introduced support for PathParam default value.

--- src/jax-rs-api/src/main/java/javax/ws/rs/DefaultValue.java
+++ src/jax-rs-api/src/main/java/javax/ws/rs/DefaultValue.java
@@ -45,33 +45,35 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Defines the default value of request metadata that is bound using one of the
+ * Defines the default value of request meta-data that is bound using one of the
  * following annotations:
+ * {@link javax.ws.rs.PathParam},
  * {@link javax.ws.rs.QueryParam},
  * {@link javax.ws.rs.MatrixParam},
  * {@link javax.ws.rs.CookieParam},
  * {@link javax.ws.rs.FormParam},
  * or {@link javax.ws.rs.HeaderParam}.
- * The default value is used if the corresponding metadata is not present in the
+ * The default value is used if the corresponding meta-data is not present in the
  * request.
- *
- * <p>If the type of the annotated parameter is
- * <code>List</code>, <code>Set</code> or <code>SortedSet</code> then the
- * resulting collection will have a single entry mapped from the supplied
- * default value.</p>
- *
- * <p>If this annotation is not used and the corresponding metadata is not
+ * <p/ >
+ * If the type of the annotated parameter is {@link List}, {@link Set} or
+ * {@link SortedSet} then the resulting collection will have a single entry mapped
+ * from the supplied default value.
+ * <p />
+ * If this annotation is not used and the corresponding meta-data is not
  * present in the request, the value will be an empty collection for
- * <code>List</code>, <code>Set</code> or <code>SortedSet</code>, null for
- * other object types, and the Java-defined default for primitive types.</p>
+ * {@code List}, {@code Set} or {@code SortedSet}, {@code null} for
+ * other object types, and the Java-defined default for primitive types.
  *
  * @author Paul Sandoz
  * @author Marc Hadley
+ * @see PathParam
  * @see QueryParam
  * @see FormParam
  * @see HeaderParam
  * @see MatrixParam
  * @see CookieParam
+ *
  * @since 1.0
  */
 @Target({ElementType.PARAMETER, ElementType.METHOD, ElementType.FIELD})
