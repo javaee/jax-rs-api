241ba9e188f433d0e98308cbe9563578755baade
Marek Potociar <marek.potociar@oracle.com>
2012-05-03 17:42:13 +0200

    Fixed custom raw type handling and toString output in GenericType class.

--- src/jax-rs-api/src/main/java/javax/ws/rs/core/GenericType.java
+++ src/jax-rs-api/src/main/java/javax/ws/rs/core/GenericType.java
@@ -176,10 +176,12 @@ public abstract class GenericType<T> {
     @SuppressWarnings("unchecked")
     public final Class<T> getRawType() {
         if (rawType == null) {
-
-            // Get the actual type
-            Type t = getType();
-            return (Class<T>) getRawType(t);
+            rawType = _rawType();
+            if (rawType == null) {
+                // Get the actual type
+                Type t = getType();
+                rawType = (Class<T>) getRawType(t);
+            }
         }
 
         return rawType;
@@ -217,7 +219,7 @@ public abstract class GenericType<T> {
      * Return the class that directly extends the {@code GenericType<T>} class
      * in the superclass hierarchy of the supplied {@code clazz} class.
      *
-     * @param class instance to inspect.
+     * @param clazz instance to inspect.
      * @return the direct descendant of {@code GenericType<T>} in the hierarchy
      *     of the supplied class.
      * @exception IllegalArgumentException if the provided class does not extend
@@ -277,6 +279,6 @@ public abstract class GenericType<T> {
 
     @Override
     public String toString() {
-        return "TypeLiteral{" + getType().toString() +"}";
+        return "GenericType{" + getType().toString() +"}";
     }
 }
