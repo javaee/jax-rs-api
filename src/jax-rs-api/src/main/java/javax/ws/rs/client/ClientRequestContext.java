eb96a8149c9753f1083d5dc9f5ef84f9f708ee9a
Marek Potociar <marek.potociar@oracle.com>
2012-09-17 17:19:15 +0200

    Updated ClientRequestContext.getStringHeaders() javadoc.

--- src/jax-rs-api/src/main/java/javax/ws/rs/client/ClientRequestContext.java
+++ src/jax-rs-api/src/main/java/javax/ws/rs/client/ClientRequestContext.java
@@ -176,8 +176,7 @@ public interface ClientRequestContext {
      * Get a string view of header values associated with the message.
      *
      * Changes in the underlying {@link #getHeaders() headers map} are reflected
-     * in this view. Conversely, this view supports removal operations, and these
-     * are reflected in the underlying headers map.
+     * in this view.
      * <p>
      * The method converts the non-string header values to strings using a
      * {@link javax.ws.rs.ext.RuntimeDelegate.HeaderDelegate} if one is available via
