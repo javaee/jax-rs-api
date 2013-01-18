f4db089f5bdf04d40d5170f9307e54724776a7f7
Marek Potociar <marek.potociar@oracle.com>
2013-01-18 20:25:51 +0100

    Added Priorities content.
    - added content of the new Priorities file that I forgot to include with last commit.
    
    Signed-off-by: Marek Potociar <marek.potociar@oracle.com>

--- src/jax-rs-api/src/main/java/javax/ws/rs/Priorities.java
+++ src/jax-rs-api/src/main/java/javax/ws/rs/Priorities.java
@@ -40,9 +40,50 @@
 package javax.ws.rs;
 
 /**
- * TODO: javadoc.
+ * A collection of built-in priority constants for the JAX-RS components that are supposed to be
+ * ordered based on their {@link javax.annotation.Priority priority} when used or applied
+ * by JAX-RS runtime.
+ * <p>
+ * For example, JAX-RS filters and interceptors are grouped in chains for each of the message
+ * processing extension points: Pre, PreMatch, Post as well as ReadFrom and WriteTo.
+ * Each of these chains is sorted based on priorities which are represented as integer numbers.
+ * All chains, except Post, are sorted in ascending order; the lower the number the higher the priority.
+ * The Post filter chain is sorted in descending order to ensure that response filters are executed in
+ * <em>reverse order</em>.
+ * </p>
+ * <p>
+ * JAX-RS components that belong to the same priority class (same integer value) are executed in an
+ * implementation-defined manner. By default, when the {@code @Priority} annotation is absent on a component,
+ * for which a priority should be applied, the {@link Priorities#USER} priority value is used.
+ * </p>
  *
  * @author Marek Potociar (marek.potociar at oracle.com)
+ * @since 2.0
  */
-public class Priorities {
+public final class Priorities {
+
+    private Priorities() {
+        // prevents construction
+    }
+
+    /**
+     * Security authentication filter/interceptor priority.
+     */
+    public static final int AUTHENTICATION = 1000;
+    /**
+     * Security authorization filter/interceptor priority.
+     */
+    public static final int AUTHORIZATION = 2000;
+    /**
+     * Header decorator filter/interceptor priority.
+     */
+    public static final int HEADER_DECORATOR = 3000;
+    /**
+     * Message encoder or decoder filter/interceptor priority.
+     */
+    public static final int ENTITY_CODER = 4000;
+    /**
+     * User-level filter/interceptor priority.
+     */
+    public static final int USER = 5000;
 }
